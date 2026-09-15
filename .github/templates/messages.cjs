5127675d5066a1df423402739a0378d9492958c4
てんとう虫 <reservecrate@gmail.com>
2026-09-15 20:13:56 +0500

    chore(automation): clarify good first issue titles

--- .github/templates/messages.cjs
+++ .github/templates/messages.cjs
@@ -221,7 +221,7 @@ module.exports = {
     common: {
       titleTemplate:
         // '[Good First Issue] {emoji} Add new {issueType} - Beginner-Friendly Contribution (good-first-issue, <1 min, no setup)',
-        '[Good First Issue] {emoji} Add new {issueType} - Beginner-Friendly Open-source Contribution',
+        '[Good First Issue] {emoji} Add new {issueType} - Beginner-Friendly Open-source Contribution (good-first-issue)',
       shortTitleTemplate: '{emoji} Add new {issueType} {id}',
       difficulty: 'Easy (good first issue!)',
       instructionsHeader: '### 📝 Instructions',
